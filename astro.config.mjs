146e4c160355cadc44ee51756748aa6258769bb9
louis17782 <luissanteliz22@gmail.com>
2026-03-30 16:32:53 -0400

    configure adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://freshvory.netlify.app/",
+  prefetch: true,
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Add valid SCSS options here if needed
+        }
+      }
+    },
+    server: {
+      hmr: true,
+    }
+  },
+});
